1d9ba5fe63ec705e9f084dc281fbc1d006f2d536
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2020-05-11 18:48:43 +0100

    DAC6-79: Create business matching microservice

---
+++ test/generators/ModelGenerators.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package generators
+
+import java.time.LocalDate
+
+import models.{Individual, IndividualMatchingSubmission, Name}
+import org.scalacheck.{Arbitrary, Gen}
+import uk.gov.hmrc.domain.Nino
+import org.scalacheck.Arbitrary.arbitrary
+
+trait ModelGenerators {
+ self: Generators =>
+
+  implicit val arbitraryName: Arbitrary[Name] = Arbitrary {
+    for {
+      firstName <- stringsWithMaxLength(50)
+      secondName <- stringsWithMaxLength(50)
+    } yield Name(firstName, secondName)
+  }
+
+  implicit val arbitraryNino: Arbitrary[Nino] = Arbitrary{
+    for {
+      prefix <- Gen.oneOf(Nino.validPrefixes)
+      number <- Gen.choose(0, 999999)
+      suffix <- Gen.oneOf(Nino.validSuffixes)
+    } yield Nino(f"$prefix$number%06d$suffix")
+  }
+
+  implicit lazy val arbitraryLocalDate: Arbitrary[LocalDate] = Arbitrary {
+    datesBetween(LocalDate.of(1900, 1, 1), LocalDate.of(2100, 1, 1))
+  }
+
+  implicit val arbitraryIndividualMatchingSubmission: Arbitrary[IndividualMatchingSubmission] = Arbitrary {
+    for {
+      name <- arbitrary[Name]
+      dob <- arbitrary[LocalDate]
+    } yield
+      IndividualMatchingSubmission("Don't know",
+        requiresNameMatch = true,
+        isAnAgent = false,
+        Individual(name, dob))
+  }
+}

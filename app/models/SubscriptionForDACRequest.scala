41bd5c296f338f9a33f2602f59044641d24c2d27
soljeggers <35451762+soljeggers@users.noreply.github.com>
2020-09-23 13:51:53 +0100

    DAC6-288 - changed models

--- app/models/SubscriptionForDACRequest.scala
+++ app/models/SubscriptionForDACRequest.scala
@@ -1,6 +1,6 @@
 package models
 
-import play.api.libs.json.{Json, OWrites, Reads, __}
+import play.api.libs.json.Json
 
 case class SubscriptionForDACRequest(requestDetail: RequestDetail)
 
@@ -8,56 +8,38 @@ object SubscriptionForDACRequest {
   implicit val format = Json.format[SubscriptionForDACRequest]
 }
 
-case class OrganisationName(name: String)
+case class Organisation(name: String)
 
-object OrganisationName {
-  implicit val format = Json.format[OrganisationName]
+object Organisation {
+  implicit val format = Json.format[Organisation]
 }
 
-case class PrimaryContact(individual: Name, email: String, phone: String, mobile: String)
-object PrimaryContact {
-  implicit lazy val writes: OWrites[PrimaryContact] = OWrites[PrimaryContact] {
-    primaryContact =>
-      Json.obj(
-        "firstName" -> primaryContact.individual.firstName,
-        "lastName" -> primaryContact.individual.secondName,
-        "email" -> primaryContact.email,
-        "phone" -> primaryContact.phone,
-        "mobile" -> primaryContact.mobile,
-      )
-  }
+case class Individual(firstName: String, middleName: Option[String], lastName: String)
 
-  implicit lazy val reads: Reads[PrimaryContact] = {
-    import play.api.libs.functional.syntax._
-    (
-      (__ \ "firstName").read[String] and
-        (__ \ "lastName").read[String] and
-          (__ \ "email").read[String] and
-            (__ \ "phone").read[String] and
-              (__ \ "mobile").read[String]
-      )((firstName, secondName, email, phone, mobile) => PrimaryContact(Name(firstName, secondName), email, phone, mobile))
-  }
+object Individual {
+  implicit val format = Json.format[Individual]
 }
 
-case class SecondaryContact(organisation: OrganisationName, email: String, phone: String)
-object SecondaryContact {
-  implicit lazy val writes: OWrites[SecondaryContact] = OWrites[SecondaryContact] {
-    secondaryContact =>
-      Json.obj(
-        "organisation" -> secondaryContact.organisation.name,
-        "email" -> secondaryContact.email,
-        "phone" -> secondaryContact.phone
-      )
-  }
+case class ContactInformation(email: String,
+                              phone: Option[String],
+                              mobile: Option[String],
+                              individual: Option[Individual],
+                              organisation: Option[Organisation])
 
-  implicit lazy val reads: Reads[SecondaryContact] = {
-    import play.api.libs.functional.syntax._
-    (
-      (__ \ "organisation").read[String] and
-        (__ \ "email").read[String] and
-        (__ \ "phone").read[String]
-      )((organisation, email, phone) => SecondaryContact(OrganisationName(organisation), email, phone))
-  }
+object ContactInformation {
+  implicit  val format = Json.format[ContactInformation]
+}
+
+case class PrimaryContact(contactInformation: ContactInformation)
+
+object PrimaryContact{
+  implicit val format = Json.format[ContactInformation]
+}
+
+case class SecondaryContact(contactInformation: ContactInformation)
+
+object SecondaryContact{
+  implicit val format = Json.format[ContactInformation]
 }
 
 case class RequestDetail(idType: String,
@@ -65,7 +47,7 @@ case class RequestDetail(idType: String,
                          tradingName: String,
                          isGBUser: Boolean,
                          primaryContact: PrimaryContact,
-                         secondaryContact: SecondaryContact)
+                         secondaryContact: Option[SecondaryContact])
 object RequestDetail {
   implicit val format = Json.format[RequestDetail]
 }
